e2d3c0083420275c26769107a2fbcae4cf89cbeb
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-15 16:36:18 +0100

    Add streams data in util file

---
+++ client/src/utils/streams.js
@@ -0,0 +1,17 @@
+import awesome80s from "../assets/awesome-80s.jpg";
+import eagleClassic from "../assets/eagle-classic.jpg";
+
+export default [
+  {
+    id: 0,
+    name: "The Eagle Classic",
+    image: awesome80s,
+    url: "181-eagle_128k.mp3",
+  },
+  {
+    id: 1,
+    name: "Awesome 80's",
+    image: eagleClassic,
+    url: "181-awesome80s_128k.mp3",
+  },
+];
