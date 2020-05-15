f89e39ad10aa5050abbb88aa1d192b3661ac9c59
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-15 16:36:40 +0100

    Write test and code to show stream image

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import streams from "../utils/streams";
 
 const App = () => {
+  const [selectedStreamIndex, setSelectedStreamIndex] = useState(0);
+
   return (
     <div>
-      <div>App</div>
+      <img
+        data-test="stream-image"
+        src={streams[selectedStreamIndex].image}
+        alt="radio"
+        height="500"
+      ></img>
     </div>
   );
 };
--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -2,9 +2,19 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
 describe("App", () => {
   test("renders without errors", () => {
     const wrapper = shallow(<App />);
     expect(wrapper.length).toEqual(1);
+  });
+
+  test("renders the stream's image", () => {
+    const wrapper = shallow(<App />);
+    const img = findByTestAttr(wrapper, "stream-image");
+    expect(img.length).toEqual(1);
   });
 });
