f884af0bd2d56cb912d5b96e780ec2a865849f02
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-05-15 17:28:28 +0100

    Setup Heroku

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ io.on("connection", (socket) => {
     io.sockets.emit("streamSelected", data);
   });
 });
+
+// Setup for Production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
